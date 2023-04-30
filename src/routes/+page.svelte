9c4ca498ef0060a865a31ad43b31cb30d6a5da33
Lucho <lucianomartin2015nqn@gmail.com>
2023-04-30 20:27:11 -0300

    Comit inicial

---
+++ src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<script>
+    export let data;
+</script>
+
+<p>HOME</p>
+
+<p>{data.dato_1.nombre}</p>
---
+++ src/routes/settings/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+    export let data;
+</script>
+
+<p>Setting</p>
+
+<form method="post">
+    <label>
+        Email
+        <input name="email" type="text" value={data.dato_1.nombre}/>
+    </label>
+    <input type="submit" value = "enviar"/>
+</form>
