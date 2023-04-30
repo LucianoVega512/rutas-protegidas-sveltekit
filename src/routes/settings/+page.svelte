34e7288d203d55b1aa3045ed609ae1d318f5a515
Lucho <lucianomartin2015nqn@gmail.com>
2023-04-30 20:47:26 -0300

    Funcion para inicar aplicacion

--- src/routes/settings/+page.svelte
+++ src/routes/settings/+page.svelte
@@ -1,13 +1,20 @@
 <script>
     export let data;
+
+    let valor = data.dato_1.nombre
+    
+    //Enviar dato a la API
+    function enviar(){
+        console.log(valor);
+    }
 </script>
 
 <p>Setting</p>
 
-<form method="post">
+<!-- <form method="POST"> -->
     <label>
         Email
-        <input name="email" type="text" value={data.dato_1.nombre}/>
+        <input name="email" type="text" bind:value={valor}/>
     </label>
-    <input type="submit" value = "enviar"/>
-</form>
+    <button on:click={enviar}>Enviar</button>
+<!-- </form> -->
